f869a4699eae5a0a31df5550b084f03219a51657
Tiafk <akchurin227@mail.ru>
2023-09-30 00:29:39 +0300

    desktop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import About from './components/About/About';
+import Contacts from './components/Contacts/Contacts';
 import Home from './components/Home/Home';
 import Worcks from './components/Worcks/Worcks';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Home/>
       <About/>
-      <Worcks />
+      <Worcks/>
+      <Contacts/>
     </div>
   );
 }
